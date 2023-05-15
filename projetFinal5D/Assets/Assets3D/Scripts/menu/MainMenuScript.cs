9dc7a08eb0bab473e9e87033665b056213bd2e75
efournierrobert <efournierrobert@dept-info.crosemont.quebec>
2023-05-14 20:02:59 -0400

    Fait le menu du projets 5D

--- projetFinal5D/Assets/Assets3D/Scripts/menu/MainMenuScript.cs
+++ projetFinal5D/Assets/Assets3D/Scripts/menu/MainMenuScript.cs
@@ -6,11 +6,23 @@ using UnityEngine.SceneManagement;
 public class MainMenuScript : MonoBehaviour
 {
     public int sceneBuildIndex;
+    public int startScene2d;
+    public int startScene3d;
 
     public void playGame()
     {
         Debug.Log("PLAY !");
         SceneManager.LoadScene(sceneBuildIndex, LoadSceneMode.Single);
+    }
+
+    public void start2d()
+    {
+        SceneManager.LoadScene(startScene2d, LoadSceneMode.Single);
+    }
+
+    public void start3d()
+    {
+        SceneManager.LoadScene(startScene3d, LoadSceneMode.Single);
     }
 
     public void quitApplication()
